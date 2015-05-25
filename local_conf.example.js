bcf51220e0fc501684cfcb11b3387af039db02ed
Tricia <totricia@gmail.com>
2015-05-25 16:10:51 -0700

    Update local_conf example for party games

--- local_conf.example.js
+++ local_conf.example.js
@@ -1,16 +1,16 @@
 /**
  * Local configuration overrides that are ignored by git.
  *
- * Use this to store local overrides or sensitive configuration data like 
+ * Use this to store local overrides or sensitive configuration data like
  * usernames and passwords.
  *
  * Copy to local_conf.js to use this file.
  */
 var conf = {
   dbHost: 'localhost',
-  dbUser: 'bookmarks_user',
-  dbPass: 'bookmarks_pw',
-  dbName: 'bookmarks'
+  dbUser: 'user',
+  dbPass: 'pw',
+  dbName: 'partygames'
 };
 
 module.exports = conf;
