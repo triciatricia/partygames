bfe03fd19e03e1e672e654838d2c06a22a0de13b
Tricia <totricia@gmail.com>
2018-01-05 22:09:14 -0800

    Remove some Express default code
    Users was never used

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -26,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(['/', '/api/*'], routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
